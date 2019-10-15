689423fcaf629121c2af8684f8a03079acc71652
alexander-makarevich <alexander.s.makarevich@gmail.com>
2019-10-15 15:11:12 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-9dt6fr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-9dt6fr)
---
+++ index.ts
@@ -0,0 +1,19 @@
+const s = 'f:v';
+
+const rules = {
+  F: ['f'],
+  V: ['v'],
+  C: ['', 'F:V'],
+}
+
+function parse(s: string): void {
+  debugger;
+}
+parse(s);
+
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
